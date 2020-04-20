39a4a38945dc0e3b9a742840ba6cae84a0d77c04
kiofarias <kiofarias@gmail.com>
2020-04-20 10:59:17 -0300

    get index providers

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -13,5 +14,5 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
-
+routes.get('/providers', ProviderController.index);
 export default routes;
