1f1ebd82cd828c246bd82c700309a300181d4f20
kiofarias <kiofarias@gmail.com>
2020-04-22 10:48:25 -0300

    Update appointment ctrl validation

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { parseISO, startOfHour } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -24,11 +25,34 @@ class AppointmentController {
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
     }
+    /* 
+    Check if start time is a possible appointment
+     */
+    const startHour = startOfHour(parseISO(date));
+    if (startHour < new Date()) {
+      res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+    /* 
+    Check if start time is a possible appointment to that provider in database
+     */
+    const checkAvaliability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: startHour,
+      },
+    });
+
+    if (checkAvaliability) {
+      res
+        .status(400)
+        .json({ error: 'Appointment hour at date is not available' });
+    }
 
     const appointment = await Appointment.create({
       user_id: req.UserId,
       provider_id,
-      date,
+      date: startHour,
     });
     res.json(appointment);
   }
